bf3aece9395aeaf17df75202496c7f293ce3791f
nour-logstic <88410368+nour-derwich@users.noreply.github.com>
2022-10-27 18:37:15 +0200

    plyground

--- Python-Flask/Week1/Day4/practice-assignment/server.py
+++ Python-Flask/Week1/Day4/practice-assignment/server.py
@@ -1,25 +1,49 @@
-from flask import Flask 
+from flask import Flask, render_template 
 app = Flask(__name__)   
 
 
 
 
-@app.route('/')         
-def hello_world():
-    return 'Hello World!'  
+# @app.route('/')         
+# def hello_world():
+#     return 'Hello World!'  
 
-@app.route('/dojo')         
-def dojo():
-    return 'Dojo!'  
+# @app.route('/dojo')         
+# def dojo():
+#     return 'Dojo!'  
 
-@app.route('/say/<name>')
-def say(name):
+# @app.route('/say/<name>')
+# def say(name):
    
-    return f'hi {name}'
+#     return f'hi {name}'
 
-@app.route('/repeat/<int:HINT>/<string:name>')
-def repeat(HINT, name):
-    return f'{name * HINT }'
+# @app.route('/repeat/<int:HINT>/<string:name>')
+# def repeat(HINT, name):
+#     return f'{name * HINT }'
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     # note that we set the 404 status explicitly
+#     return render_template('404.html'), 404
+
+#playground >>>>>>>
+@app.route('/<int:HINT>/<string:color>')         
+def index(HINT,color):
+    return render_template("index.html", times=HINT,color=color)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
